114ef4ad024364cd245379ca83e1939bb3931bae
Joao <joaosilva2276@gmail.com>
2022-07-28 18:54:51 -0400

    fix

--- Lab1/Aviao.java
+++ Lab1/Aviao.java
@@ -1,4 +1,5 @@
 class Aviao {
+private String modelo;
 private String identificador;
 private Boolean motor;
 private float altura;
@@ -45,7 +46,21 @@ public void atualizarStatusVoo(){
 public void setModelo(String novoValor){
     this.modelo=novoValor;
 }
-@@ -32,5 +63,53 @@ public float getAltura(){
+public String getIdentificador(){
+    return this.identificador;
+}
+public void setIdentificador(String novoValor ){
+    this.identificador=novoValor;
+}
+public Boolean getMotor(){
+    return this.motor;
+}
+public void setMotor(boolean novoValor){
+this.motor=novoValor;
+}
+public float getAltura(){
+    return this.altura;
+}
 public void setAltura(float novoValor){
     this.altura=novoValor;
 }
--- Lab1/main.java
+++ Lab1/main.java
@@ -2,41 +2,34 @@ class Main {
 
     public static void main(String[] args){
         Aviao vacaloLaranja;
-        vacaloLaranja= new Aviao();
         vacaloLaranja= new Aviao("blue Dragon","frankstein");
         vacaloLaranja.imprimaok();
-        ;
-
-        vacaloLaranja.setModelo("Blue Dragon");
-        vacaloLaranja.setIdentificador("Freiherr");
-        vacaloLaranja.setMotor(false);
-        vacaloLaranja.setAltura(0.0f);
         System.out.println("Model:"+vacaloLaranja.getModelo()+"\nID:"+vacaloLaranja.getIdentificador()+
                             "\nStatus:"+vacaloLaranja.getMotor()+"\nAltura:"+vacaloLaranja.getAltura());        ;
-        vacaloLaranja.ligarMotor();
+        vacaloLaranja.ligarMotor();        
         vacaloLaranja.imprimeEstadoMotor();
         vacaloLaranja.desligaMotor();
         vacaloLaranja.imprimeEstadoMotor();
         vacaloLaranja.ligarMotor();
         vacaloLaranja.acelerar();
-        vacaloLaranja.acelerar();
-        vacaloLaranja.desligaMotor();
-        vacaloLaranja.acelerar();
+        vacaloLaranja.acelerar(); 
+        vacaloLaranja.desligaMotor(); 
+        vacaloLaranja.acelerar();  
 
         vacaloLaranja.ligarMotor();
-        vacaloLaranja.desacelerar();
-        vacaloLaranja.desacelerar();
-        vacaloLaranja.desacelerar();
+        vacaloLaranja.desacelerar(); 
+        vacaloLaranja.desacelerar(); 
+        vacaloLaranja.desacelerar(); 
         vacaloLaranja.desligaMotor();
-        vacaloLaranja.desacelerar();
+        vacaloLaranja.desacelerar(); 
 
 
         vacaloLaranja.ligarMotor();
+        vacaloLaranja.acelerar(); 
+        vacaloLaranja.acelerar(); 
         vacaloLaranja.acelerar();
-        vacaloLaranja.acelerar();
-        vacaloLaranja.acelerar();
-        vacaloLaranja.acelerar();
-        vacaloLaranja.acelerar();
+        vacaloLaranja.acelerar(); 
+        vacaloLaranja.acelerar(); 
 
         vacaloLaranja.desacelerar();
         vacaloLaranja.desacelerar();
